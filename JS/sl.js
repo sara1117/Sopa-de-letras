b8c6eb06daa7dd44e32dfa1816a38054393d1192
sara gonzalez <saragonzalez4421@gmail.com>
2020-10-24 15:38:09 -0500

    se agrego JavaScript a la sopa de letras

---
+++ JS/sl.js
@@ -0,0 +1,11 @@
+var color1="";
+		function color (elemento) {
+			if(color1=="#ADF1E9")
+			{
+				elemento.style.backgroundColor=color1="#73BECC";
+			}else{
+				elemento.style.backgroundColor=color1="#ADF1E9";
+			}
+		}
+		
+		
